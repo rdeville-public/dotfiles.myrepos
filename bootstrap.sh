cc111cc6522e2642575a1c84888d7d221fe4d54b
Romain Deville <code@romaindeville.fr>
2022-01-08 17:29:48 +0100

    ✨ Add boostrap script

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+NODES=(
+  ".mrconfig"
+)
+
+source "${SCRIPTPATH}/lib/mr_log.sh"
+
+for i_node in "${NODES[@]}"
+do
+  src="${SCRIPTPATH}/${i_node}"
+  dest="${HOME}/${i_node}"
+  if [[ -e "${dest}" ]] && ! [[ -L "${HOME}/.mrconfig" ]]
+  then
+    mr_log "INFO" "Backup **${dest}**."
+    mv -v "${dest}" "${dest}.bak"
+    mr_log "INFO" "Create symlinks to **${dest}**."
+    ln -s "${src}" "${dest}"
+  elif ! [[ -L "${HOME}/.mrconfig" ]]
+  then
+    mr_log "INFO" "Create symlink to **${dest}**."
+    ln -s "${src}" "${dest}"
+  else
+    mr_log "INFO" "Symlink to **${dest}** already exists."
+  fi
+done

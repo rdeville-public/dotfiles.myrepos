344f412eaf6520e0c2afc4d694f8b112a84ed23f
Romain Deville <code@romaindeville.fr>
2022-01-11 17:38:07 +0100

    ✨ Introduce options for mr_register

--- lib/mr_register.sh
+++ lib/mr_register.sh
@@ -2,13 +2,11 @@
 
 _check_git_repo(){
   local repo_name="${VCSH_REPO_NAME:-${PWD/${HOME}/\~}}"
-  if [[ -z "${VCSH_REPO_NAME}" ]] && ! [[ -d "${PWD}/.git" ]]
+  if [[ -z "${VCSH_REPO_NAME}" ]] \
+    && ! ([[ -d "${PWD}/.git" ]] \
+          || git rev-parse --is-inside-work-tree &> /dev/null)
   then
     mr_log "ERROR" "Repo **${repo_name}** is not a git repo, aborting."
-    exit 1
-  elif [[ -z "${VCSH_REPO_NAME}" ]] && ! git rev-parse --is-inside-work-tree
-  then
-    mr_log "ERROR" "Repo **${repo_name}** is not in a git repo, aborting."
     exit 1
   elif [[ $(git remote | wc -l) -eq 0 ]]
   then
@@ -18,6 +16,9 @@ _check_git_repo(){
   then
     mr_log "ERROR" "No remote **origin** defined for **${repo_name}**, aborting."
     exit 1
+  else
+    repo_path="$(git rev-parse --show-toplevel)"
+    export MR_REPO="$(basename ${repo_path})"
   fi
 }
 
@@ -54,40 +55,52 @@ _define_register_path(){
 
 _compute_register_content(){
   local remote_origin_url="$(git config --get remote.origin.url)"
-  local repo_path=""
-
   if [[ -n "${VCSH_REPO_NAME}" ]]
   then
     repo_path=".local/share/vcsh/repo.d/${VCSH_REPO_NAME}.git"
-  else
-    repo_path="${PWD#${HOME}/}"
   fi
 
   git_tpl="\
-[${repo_path}]
+[${repo_path/${HOME}\/}]
 checkout =
-  mr_checkout \"${remote_origin_url}\" \"\$@\"
-"
+  mr_checkout \"${remote_origin_url}\" \"\$@\""
 }
 
 
 _register_repo(){
-  if [[ -f "${dest}" ]]
+  if [[ "${option}" != "append" ]]
   then
-    mkdir -p "$(dirname ${dest_tmp})"
-    echo "${git_tpl}" > "${dest_tmp}"
-    if ! diff -q "${dest}" "${dest_tmp}"
+    if [[ -f "${dest}" ]]
     then
-      mr_log "WARNING" "Old and new registering for **${MR_REPO}** differs."
-      mr_log "WARNING" "Backup old registering for **${MR_REPO}**."
-      mv "${dest}" "${dest}.bak"
-    else
-      mr_log "INFO" "Old and new registering for **${MR_REPO}** are the same."
+      mkdir -p "$(dirname ${dest_tmp})"
+      echo "${git_tpl}" > "${dest_tmp}"
+      if ! diff -q "${dest}" "${dest_tmp}" &> /dev/null
+      then
+        mr_log "WARNING" "Old and new registering for **${MR_REPO}** differs."
+        mr_log "WARNING" "Backup old registering for **${MR_REPO}**."
+        mv "${dest}" "${dest}.bak"
+      else
+        mr_log "INFO" "Old and new registering for **${MR_REPO}** are the same."
+      fi
     fi
-  else
     mr_log "INFO" "Registering **${MR_REPO}** in **${dest/${HOME}/\~}**."
     mkdir -p "$(dirname ${dest})"
-    echo "${git_tpl}" > "${dest}"
+    echo -e "${git_tpl}\n" > "${dest}"
+  else
+    if ! [[ -f "${dest}" ]]
+    then
+      mr_log "INFO" "Registering **${MR_REPO}** in **${dest/${HOME}/\~}**."
+      mkdir -p "$(dirname ${dest})"
+      echo -e "${git_tpl}\n" >> "${dest}"
+    elif ! grep "${repo_path/${HOME}\/}" "${dest}"
+    then
+      mr_log "INFO" "Adding **${MR_REPO}** in **${dest/${HOME}/\~}**."
+      mkdir -p "$(dirname ${dest})"
+      echo -e "${git_tpl}\n" >> "${dest}"
+    else
+      mr_log "WARNING" "Repo **${MR_REPO/${HOME}/\~}** seems to already be registered in **${dest/${HOME}/\~}**."
+      mr_log "WARNING" "No new registration will be done."
+    fi
   fi
 }
 
@@ -97,7 +110,7 @@ _symlink_repo_in_host(){
 
   if ! [[ -L "${dest_host}" ]]
   then
-    mr_log "INFO" "Create symlinks for **${MR_REPO}** for **${HOSTNAME}**."
+    mr_log "INFO" "Create symlinks for **${MR_REPO/${HOME}/\~}** for **${HOSTNAME}**."
     mkdir -p "$(dirname ${dest_host})"
     dest="${dest/${dest_prefix}\//}"
     for ((i=0; i<nb_subdir; i++))
@@ -106,7 +119,7 @@ _symlink_repo_in_host(){
     done
     ln -s "${dest}" "${dest_host}"
   else
-    mr_log "INFO" "Symlinks for **${MR_REPO}** for **${HOSTNAME}** already exists."
+    mr_log "INFO" "Symlinks for **${MR_REPO/${HOME}/\~}** for **${HOSTNAME}** already exists."
   fi
 }
 
@@ -120,10 +133,11 @@ _test_option(){
 
 mr_register(){
   local dest_prefix="${XDG_DATA_DIR:-${HOME}/.local/share}/mr"
+  local append_file="repos"
   local dest_dir=""
   local dest_dir_host=""
   local option=""
-  local append_file="repos"
+  local repo_path=""
 
   while [[ $# -gt 0 ]]
   do
@@ -140,7 +154,7 @@ mr_register(){
         ;;
       --flat|-f)
         _test_option
-        option="append"
+        option="flat"
         shift
         ;;
       --dir|-d)

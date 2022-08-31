60627251591840dae94d0382a9639a4fb5738571
Romain Deville <code@romaindeville.fr>
2022-08-31 21:21:55 +0200

    🐛 Fix call to bootstrap as now not manage per dir

--- lib/bootstrap_dotfiles.sh
+++ lib/bootstrap_dotfiles.sh
@@ -2,10 +2,7 @@
 
 bootstrap_dotfiles(){
   bootstrap_script="${MR_REPO}/bootstrap.sh"
-  if ([[ "${MR_REPO}" =~ ${HOME}/.config ]] \
-      || [[ "${MR_REPO}" =~ ${HOME}/.local ]]) \
-    && [[ -e "${bootstrap_script}" ]] \
-    && [[ -x ${bootstrap_script} ]]
+  if [[ -e "${bootstrap_script}" ]] && [[ -x ${bootstrap_script} ]]
   then
     mr_log "INFO" "Bootstrap **${MR_REPO/${HOME}/\~}**."
     ${bootstrap_script}

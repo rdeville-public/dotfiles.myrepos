d1fa9a7fe8e408f24bdd46dbfe508392b45bab77
Romain Deville <code@romaindeville.fr>
2024-01-22 08:30:54 +0100

    🐛 Update _log.sh from snippets

--- lib/_log.sh
+++ lib/_log.sh
@@ -1,110 +1,84 @@
 #!/usr/bin/env bash
-# """Contain method to print log message
-#
-# SYNOPSIS:
-#   source /path/to/_log.sh
-#
-# DESCRIPTION:
-#   Echo colored log depending on user provided message severity. Message
-#   severity are associated to following color output:
-#
-#     - `DEBUG` print in the fifth colors of the terminal (usually magenta)
-#     - `INFO` print in the second colors of the terminal (usually green)
-#     - `WARNING` print in the third colors of the terminal (usually yellow)
-#     - `ERROR` print in the third colors of the terminal (usually red)
-#
-#   If no message severity is provided, severity will automatically be set to
-#   INFO.
-#
+# """Log function allowing to echo colored string depending on debug level
 # """
 
 # shellcheck disable=SC2034
 #   - SC2034: var appears unused, Verify use (or export if used externally)
 _log()
 {
-  # """Print debug message in colors depending on message severity on stderr
-  #
-  # Echo colored log depending on user provided message severity. Message
-  # severity are associated to following color output:
-  #
-  #   - `DEBUG` print in the fifth colors of the terminal (usually magenta)
-  #   - `INFO` print in the second colors of the terminal (usually green)
-  #   - `WARNING` print in the third colors of the terminal (usually yellow)
-  #   - `ERROR` print in the third colors of the terminal (usually red)
-  #
-  # If no message severity is provided, severity will automatically be set to
-  # INFO.
-  #
-  # Globals:
-  #   None
-  #
-  # Arguments:
-  #   $1: string, message severity or message content
-  #   $@: string, message content
-  #
-  # Output:
-  #   Log informations colored
-  #
-  # Returns:
-  #   None
-  #
-  # """
+# """Print debug message in colors depending on message severity on stderr
+#
+# Echo colored log depending on user provided message severity. Message
+# severity are associated to following color output:
+#   - `DEBUG` print in the fifth colors of the terminal (usually magenta)
+#   - `INFO` print in the second colors of the terminal (usually green)
+#   - `WARNING` print in the third colors of the terminal (usually yellow)
+#   - `ERROR` print in the third colors of the terminal (usually red)
+# If no message severity is provided, severity will automatically be set to
+# INFO.
+#
+# Arguments:
+#   $1: string, message severity or message content
+#   $@: string, message content
+#
+# Output:
+#   Log informations colored to stderr (to avoid catching by parent call)
+#
+# """
+  _get_level(){
+    local array=("error" "warning" "info" "debug" "time")
+    local value=$(echo $1 | tr '[:upper:]' '[:lower:]')
+
+    for idx in "${!array[@]}"; do
+    if [[ "${array[$idx]}" = "${value}" ]]; then
+      echo "${idx}";
+      return
+    fi
+    done
+    echo "-1"
+    return 1
+  }
 
   # Store color prefixes in variable to ease their use.
+  declare -A color
   # Base on only 8 colors to ensure portability of color when in tty
-  local e_normal="\033[0m"     # Normal (usually white fg & transparent bg)
-  local e_bold="\033[1m"       # Bold
-  local e_underline="\033[4m"  # Underline
-  local e_debug="\033[0;34m"   # Fifth term color (usually magenta fg)
-  local e_info="\033[0;32m"    # Second term color (usually green fg)
-  local e_warning="\033[0;33m" # Third term color (usually yellow fg)
-  local e_error="\033[0;31m"   # First term color (usually red fg)
+  color["normal"]="\e[0m"     # Normal (usually white fg & transparent bg)
+  color["bold"]="\e[1m"       # Bold
+  color["underline"]="\e[4:3m"  # Underline
+  color["debug"]="\e[34m"   # Fifth term color (usually magenta fg)
+  color["info"]="\e[32m"    # Second term color (usually green fg)
+  color["warning"]="\e[33m" # Third term color (usually yellow fg)
+  color["error"]="\e[31m"   # First term color (usually red fg)
 
-  # Store preformated colored prefix for log message
-  local error="${e_bold}${e_error}[ERROR]${e_normal}${e_error}"
-  local warning="${e_bold}${e_warning}[WARNING]${e_normal}${e_warning}"
-  local info="${e_bold}${e_info}[INFO]${e_normal}${e_info}"
-  local debug="${e_bold}${e_debug}[INFO]${e_normal}${e_debug}"
 
-  local color_output="e_error"
-  local msg_severity
-  local msg
+  local severity_lower=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+  local severity_upper=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+  local prefix="${color["bold"]}${color[$severity_lower]}[${severity_upper}]${color["normal"]}${color[$severity_lower]}"
+  local color_output="${color[$severity_lower]}"
+  local msg_level=$(_get_level "${severity_lower}")
+  local curr_level=$(_get_level "${DEBUG_LEVEL}")
+  local msg=""
 
-  # Not using ${1^^} to ensure portability when using ZSH
-  msg_severity=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-
-  if [[ "${msg_severity}" =~ ^(error|time|warning|info|debug)$ ]]
+  if [[ ${curr_level} -ge ${msg_level} ]]
   then
-    # Shift arguments by one such that $@ start from the second arguments
-    shift
-    # Place the content of variable which name is defined by ${msg_severity}
-    # For instance, if `msg_severity` is INFO, then `prefix` will have the same
-    # value as variable `info`.
-    prefix="${!msg_severity}"
-    color_output="e_${msg_severity}"
-  else
-    prefix="${info}"
+      # Concat all remaining arguments in the message content and apply markdown
+      # like syntax.
+      # Regex will find all pattern of the form **(string)** or __(string)__ and
+      # apply formating, respectively bold and underline.
+      # Note: (string) cannot have output pattern (** or __) in them, this is
+      # specified by '[^\*\*]' and '[^__]' in group selection.
+      shift
+      msg_content=$(echo "$*" | \
+      sed -r \
+          -e "s/\*\*([^\*\*]*)\*\*/\\${color["bold"]}\1\\${color["normal"]}\\${color_output}/g" \
+          -e "s/__([^__]*)__/\\${color["underline"]}\1\\${color["normal"]}\\${color_output}/g" \
+          )
+      msg="${prefix} ${msg_content}${color["normal"]}"
+      # Print message
+      echo -e "${msg}" 1>&2
+      return
   fi
-  color_output="${!color_output}"
-
-  # Concat all remaining arguments in the message content and apply markdown
-  # like syntax.
-  msg_content=$(echo "$*" | \
-    sed -e "s/ \*\*/ \\${e_bold}/g" \
-        -e "s/\*\*\./\\${e_normal}\\${color_output}./g" \
-        -e "s/\*\*$/\\${e_normal}\\${color_output}/g" \
-        -e "s/\*\*,/\\${e_normal}\\${color_output},/g" \
-        -e "s/\*\* /\\${e_normal}\\${color_output} /g" \
-        -e "s/ \_\_/ \\${e_underline}/g" \
-        -e "s/\_\_\./\\${e_normal}\\${color_output}./g" \
-        -e "s/\_\_ /\\${e_normal}\\${color_output} /g" \
-        -e "s|${HOME}|~|g" \
-
-  )
-  msg="${prefix} ${msg_content}${e_normal}"
-
-  # Print message
-  echo -e "${msg}" 1>&2
+  return
 }
 
-# vim: ft=bash: fdm=indent
+# vim: ft=bash: foldmethod=indent
